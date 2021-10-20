171827ac1ef4df9fae0f33b1db10ec7cbe402d57
kamynz16 <kamynz16@gmail.com>
2021-10-19 18:37:07 -0700

    chore: Using case function

--- 01-SQL_-_NO_SQL/PracticeHackerRank/SQL_Basic/answer_exercises.sql
+++ 01-SQL_-_NO_SQL/PracticeHackerRank/SQL_Basic/answer_exercises.sql
@@ -1,3 +1,58 @@
+-- Advanced Select - Occupations
+--https://www.hackerrank.com/challenges/occupations/forum
+set @r1=0, @r2=0, @r3=0, @r4=0;
+select min(Doctor), min(Professor), min(Singer), min(Actor)
+from(
+  select case when Occupation='Doctor' then (@r1:=@r1+1)
+            when Occupation='Professor' then (@r2:=@r2+1)
+            when Occupation='Singer' then (@r3:=@r3+1)
+            when Occupation='Actor' then (@r4:=@r4+1) end as RowNumber,
+    case when Occupation='Doctor' then Name end as Doctor,
+    case when Occupation='Professor' then Name end as Professor,
+    case when Occupation='Singer' then Name end as Singer,
+    case when Occupation='Actor' then Name end as Actor
+  from OCCUPATIONS
+  order by Name
+	) temp
+group by RowNumber;
+
+-- otra opcion,MySQL
+SELECT 
+MIN(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
+MIN(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
+MIN(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
+MIN(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
+FROM (
+  SELECT a.Occupation,
+         a.Name,
+         (SELECT COUNT(*) 
+            FROM Occupations AS b
+            WHERE a.Occupation = b.Occupation AND a.Name > b.Name) AS rank
+  FROM Occupations AS a
+) AS c
+GROUP BY c.rank;
+
+
+
+-- Advanced Select - PADs
+select concat(name,concat('(', concat(substr(occupation,1,1),')'))) from occupations order by name;
+
+select concat('There are a total of',concat(' ',concat(count(occupation),concat(' ',concat(lower(occupation),'s.'))))) as total from occupations
+group by occupation order by total;
+
+--Easy, what-type-of-triangle
+SELECT CASE
+    WHEN A + B  > C and B + C > A AND A + C > B THEN
+    CASE
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+    ELSE 'Not A Triangle'
+END
+FROM TRIANGLES;
+
+
 -- Revising the Select Query I
 SELECT *
 FROM CITY
