354e89faf278083871c04fda2a631c0224ba1a04
kamynz16 <kamynz16@gmail.com>
2021-11-09 23:46:14 -0800

    feature: Getting deeper in algo complexity

---
+++ 02-Python/00-POO_Algo_Python/Practice1/logic/algo_complexity_classes.py
@@ -0,0 +1,12 @@
+"""
+Notas:
+  -Clases de complejidad algoritmica
+
+  -O(1): Constante
+  -O(n): Lineal
+  -O(log n): Logaritmico
+  -O(n log n): Logaritimico lineal
+  -O(n**2): Ojo, ese es n a la potencia
+  -O(2**n): Ojo, esto es 2 a la N potencia, no a la N potencia.
+
+"""
